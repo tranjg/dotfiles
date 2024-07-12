a5e6b990d1eba42c7b10867f21d247496d62f52f
tranjg <justintranthedeveloper@gmail.com>
2024-07-11 20:16:53 -0400

    added autoclose plugin

--- .config/nvim/lua/plugins/autoclose.lua
+++ .config/nvim/lua/plugins/autoclose.lua
@@ -1,31 +1,6 @@
 return {
   "m4xshen/autoclose.nvim",
-
-  config == function()
-    local autoclose = require("autoclose")
-    autoclose.setup({
-      keys = {
-        ["("] = { escape = false, close = true, pair = "()" },
-        ["["] = { escape = false, close = true, pair = "[]" },
-        ["{"] = { escape = false, close = true, pair = "{}" },
-
-        [">"] = { escape = true, close = false, pair = "<>" },
-        [")"] = { escape = true, close = false, pair = "()" },
-        ["]"] = { escape = true, close = false, pair = "[]" },
-        ["}"] = { escape = true, close = false, pair = "{}" },
-
-        ['"'] = { escape = true, close = true, pair = '""' },
-        ["'"] = { escape = true, close = true, pair = "''" },
-        ["`"] = { escape = true, close = true, pair = "``" },
-      },
-      options = {
-        disabled_filetypes = { "text" },
-        disable_when_touch = false,
-        touch_regex = "[%w(%[{]",
-        pair_spaces = false,
-        auto_indent = true,
-        disable_command_mode = false,
-      },
-    })
+  config = function()
+    require("autoclose").setup()
   end,
 }
